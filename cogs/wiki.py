e6ec118ccda09462adaafbb22c874170e9d08256
Taylor Brett <taylor.brett89@gmail.com>
2019-03-18 00:52:51 -0400

    swapped to using mediawiki package over raw api

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -1,13 +1,35 @@
 import requests
 import urllib
+import codecs
 
 from pprint import pformat, pprint
+from discord import Embed
+
+from mediawiki import MediaWiki
+
+wiki = MediaWiki()
 
 def get_wiki_page(title):
-    title = title.replace(' ', '_')
-    return requests.get(('https://en.wiipedia.org/api/rest_v1/page/summary/%s'
-                            % urllib.parse.quote_plus(title)))
+    wiki_search = wiki.search(title)[0]
+    return wiki.page(wiki_search)
+    
 
-                            
+def build_wiki_embed(wiki_page):
+    embed_title = wiki_page.title
 
+    embed=Embed(title=embed_title)
 
+    # TODO: How to fix decode of summary?
+    #   Works on most pages, but pages like "Saint Patrick's Day" break.
+    #   What exactly is causing it to break? 
+    # decode_summary = wiki_page.summary.encode('ascii', 'ignore').decode('ascii')
+    # decode_summary = decode_summary.replace("\"", '\'')
+    # decode_summary = decode_summary.replace("\n", '')
+    
+    # embed.add_field(name="Summary", value=decode_summary, inline=True)
+    
+    embed.set_thumbnail(url=wiki_page.logos[0])
+    embed.add_field(name="URL", 
+                    value=wiki_page.url,
+                    inline=True)
+    return embed
