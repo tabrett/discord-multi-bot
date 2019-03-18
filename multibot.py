da1eece251221b01c9ff26bcdd774b1ec0c2a050
Taylor Brett <taylor.brett89@gmail.com>
2019-03-17 22:27:47 -0400

    reorganization and general cleaup. added todos

--- multibot.py
+++ multibot.py
@@ -8,25 +8,29 @@ import datetime
 import asyncio
 import aiohttp
 
+from pprint import pformat
 from random import ( randint, seed )
 
-# from discord import ( Embed, Client )
-import discord
+# import discord
+from discord import ( Embed, Client, ChannelType )
+from discord import utils as disc_utils
+
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-from cogs.rng.dice_roll import *
-from cogs.rng.coinflip import *
-
-from cogs import help as help_cog
-from cogs import info as info_cog
-# from cogs import rng as rng_cog
 from cogs import channel_move as cmove_cog
 
-from settings.config import Config as config
+from cogs.wiki import get_wiki_page
+from cogs.meta import time_delta, build_info_message
+from cogs.help import build_help_message, get_help_imbed
+
+from cogs.coinflip import coin_flip
+from cogs.dice_roll import build_roll_result
+
+from cogs.config import Config as config
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 # handler.setFormatter(logging.Formatter('%(asctime)s:%s(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
@@ -34,25 +38,12 @@ logger.addHandler(handler)
 # client = Client(command_prefix)
 bot = Bot(description='MultiBot', command_prefix=config.COMMAND_PREFIX)
 
-start_time = datetime.datetime.now()                                            
+start_time = datetime.datetime.now()
 
 
 #=========================
 # command listeners
 #=========================
-@bot.command(pass_context=True)
-async def prefix(ctx):
-    if 'set' in ctx.message.content:
-        # print(ctx.message.content)
-        msg_content = ctx.message.content.split(" ")
-        # print(msg_content)
-        bot.command_prefix = msg_content[2]
-        config.COMMAND_PREFIX = msg_content[2]
-        await bot.say("Bot command prefix set to: '%s'" % config.COMMAND_PREFIX)
-    else:
-        await bot.say("Current bot command prefix: '%s'" % config.COMMAND_PREFIX)
-
-
 @bot.command(aliases=['flip'])
 async def coinflip():
     """Returns result of a heads/tails coin flip"""
@@ -70,11 +61,14 @@ async def available_channels(ctx):
 
 @bot.command(pass_context=True)
 async def uptime(ctx):
-    await bot.say(info_cog.time_delta(datetime.datetime.now() 
+    """Returns the uptime of the bot."""
+    await bot.say(time_delta(datetime.datetime.now() 
                                      -start_time))
 
 @bot.command(pass_context=True)
 async def ping(ctx):
+    """Pings the bot and returns the response time."""
+
     channel = ctx.message.channel
     t1 = time.perf_counter()
     await bot.send_typing(channel)
@@ -84,8 +78,15 @@ async def ping(ctx):
 
 @bot.command(pass_context=True)
 async def roll(ctx, *args):
+    """Returns the result of a dice roll.
+    Ex: !roll 2d20 - Returns result of rolling 2 d20.
 
-    dice_val, dice_count = roll_parse(ctx.message.content)
+    Roll split defaults to 'd', but can be changed in configuration.
+
+    Maximum of 15 dice per roll.  This can be changed in configuration."""
+
+    roll = ctx.message.content.split(' ')[1]
+    dice_count, dice_val = map(str, roll.split(config.ROLL_SPLIT))
     hit_cap = False
     
     # hard cap rolls at rollCap value
@@ -96,30 +97,22 @@ async def roll(ctx, *args):
     except:
         dice_count = 0
 
-    # returns help text first
-    if dice_val.lower() in config.BOT_COMMANDS:
-        if (dice_val.lower() == 'commands' or
-            dice_val.lower() == 'help'):
-            help_table = help_cog.build_help_message()
-
-            await bot.say(help_table) # embed=get_HelpImbed(help_table))
-        elif dice_val.lower() == 'info':
-            await bot.say(embed=info_cog.build_info_message())
-
-    elif int(dice_val) in config.VALID_DICE:
-        await bot.say(embed=build_RollResult( ctx.message.author.name, 
+    if int(dice_val) in config.VALID_DICE:
+        await bot.say( '<@%s>' % ctx.message.author.id )
+        await bot.say(embed=build_roll_result( ctx.message.author.name, 
                                                 int(dice_val), 
                                                 int(dice_count), 
                                                 hit_cap ) )
     else:
-        msg = 'You fucked up. Pick a valid die or command.'
+        msg = 'That was not a valid die. Pick a valid die or command.'
         await bot.say(msg)
-        await bot.say(embed=help_cog.get_help_imbed(help_table))
 
 
 @bot.command(pass_context=True, aliases=['grantbot'])
 async def grant_bot(ctx):
-    # message.content.startswith(': fire')):
+    """Low-effort "meme."
+    
+    Responds *just* like Grant."""
     msg = ': fire'
     time.sleep(config.BOT_RESPONSE_DELAY)
     await bot.add_reaction(ctx.message, u'\U0001F525')
@@ -128,6 +121,9 @@ async def grant_bot(ctx):
 
 @bot.command(pass_context=True, aliases=['michaelbot'])
 async def michael_bot(ctx):
+    """Low-effort "meme."
+    
+    Responds *just* like Michael."""
     # message.content.startswith(':0')):
     msg = ':0'
     time.sleep(config.BOT_RESPONSE_DELAY)
@@ -137,6 +133,9 @@ async def michael_bot(ctx):
 
 @bot.command(pass_context=True, aliases=['telorbot','taytaybot'])
 async def taylor_bot(ctx):
+    """Low-effort "meme."
+    
+    Responds *just* like Tay."""
     msg = 'No u'
     time.sleep(config.BOT_RESPONSE_DELAY)
     await bot.say(msg)
@@ -144,6 +143,7 @@ async def taylor_bot(ctx):
 
 @bot.command(pass_context=True, aliases=['chibot','chihuahuabot'])
 async def chihuahua_bot(ctx):
+    """Low-effort meme image."""
     time.sleep(config.BOT_RESPONSE_DELAY)
     # embed.set_image(url="https://imgur.com/a/tiW7M9A")
     await bot.send_file(ctx.message.channel, "assets/im-sorry.png")
@@ -151,13 +151,21 @@ async def chihuahua_bot(ctx):
 
 @bot.command(pass_context=True)
 async def raid(ctx):
-    landing_channel = discord.utils.get(ctx.message.server.channels,
-                                        name=config.LANDING_CHANNEL_NAME,
-                                        type=discord.ChannelType.voice)
+    """Moves all users from one voice channel to another.
 
-    target_channel = discord.utils.get(ctx.message.server.channels,
-                                       name=config.TARGET_CHANNEL_NAME,
-                                       type=discord.ChannelType.voice)
+    Channels are designated in the bot configuration, 
+
+    LANDING_CHANNEL_NAME: The starting channel.
+    TARGET_CHANNEL_NAME: The channel to move users to.
+    """
+
+    landing_channel = disc_utils.get(ctx.message.server.channels,
+                                     name=config.LANDING_CHANNEL_NAME,
+                                     type=ChannelType.voice)
+
+    target_channel = disc_utils.get(ctx.message.server.channels,
+                                    name=config.TARGET_CHANNEL_NAME,
+                                    type=ChannelType.voice)
     
     i = 0
     for member in landing_channel.voice_members:
@@ -170,27 +178,74 @@ async def raid(ctx):
     await bot.say('%s users moved.' % i)
 
 
-# @bot.command(pass_context=True, aliases=['config', 'botconfig'])
-# async def bot_config(ctx, *args):
-#     await bot.say(args)
+@bot.command(pass_context=True, aliases=['wikibot'])
+async def wiki(ctx, *args):
+    """Queries the Wikipedia API and returns a summary of the page passed as
+    an argument.  
 
-#     config_list = [ (k, config.__dict__[k]) for k in config.__dict__
-#                             if not k.startswith('_')]
-    
-#     for item in config_list:
-#         await bot.say(item)
+    Returns similar pages if none found."""
+    page_title = ctx.message.content.split(" ")[1]
+    get_wiki_page(page_title).json()
 
-    # for k in config.__dict__:
-    #     if not k.startswith('_'):
-    #         # msg
-    #         try:
-    #             msg = '\n'.join('%s : %s' %(k, config.__dict__[k]))
-    #             await bot.say(msg)
-    #             # await bot.say('%s : %s\n' % (k, config.__dict__[k]))
-    #         # except AttributeError:
-    #             # await bot.say('%s : %s\n' % (k, config.__dict__[k].encode("utf-8")))
-    #         except:
-    #             pass
+
+@bot.command(pass_context=True, aliases=['config', 'botconfig'])
+async def bot_config(ctx, *args):
+    """Lists the current configuration values for the bot."""
+    config_items = { k:v for (k,v) in config.__dict__.items()
+                        if not k.startswith('_') }
+
+    config_str = ""
+    for item in config_items:
+        if isinstance(config_items[item], list):
+            list_str = pformat(config_items[item])
+            config_str +=("%s: \n %s\n\n" % (item, list_str))
+        else:
+            config_str += ("%s: %s\n\n" % (item, config_items[item]))
+
+    embed_config = Embed(title="Current Configuration")
+    embed_config.add_field(name=u"\u200b", value=config_str, inline=False)
+
+    # TODO: change to private message
+    await bot.say(embed=embed_config)
+
+
+@bot.command(pass_context=True)
+async def prefix(ctx):
+    """Shows the current command prefix configuration.
+    Change the prefix with '!prefix set <new_prefix>'."""
+    if 'set' in ctx.message.content:
+        if ctx.message.author.server_permissions.administrator:
+            msg_content = ctx.message.content.split(" ")
+            
+            bot.command_prefix = msg_content[2]
+            config.COMMAND_PREFIX = msg_content[2]
+
+            logger.info("Bot command prefix set to: '%s'" % config.COMMAND_PREFIX)
+            await bot.say("Bot command prefix set to: '%s'" % config.COMMAND_PREFIX)
+        else: 
+            await bot.say("You don't have permission to do this.")
+    else:
+        await bot.say("Current bot command prefix: '%s'" % config.COMMAND_PREFIX)
+
+
+# TODO: Code to change dice roll splitter
+# TODO: Code to change Roll Cap
+# TODO: Code to add/modify valid dice list
+# TODO: Code to add/modify AT_EVERYONE_RESPONSES
+# TODO: Code to modify LANDING_CHANNEL_NAME/TARGET_CHANNEL_NAME
+# TODO: Code to modify BOT_RESPONSE_DELAY
+
+
+@bot.command(pass_context=True, aliases=['code', 'source', 'git', 'github', 'scm'])
+async def bot_code(ctx):
+    """Link to source code GitHub repo."""
+    await bot.say("https://github.com/tabrett/discord-multi-bot")
+
+
+@bot.command(pass_context=True, aliases=['docs', 'apidocs'])
+async def bot_docs(ctx):
+    """Link to Discord.py API reference docs."""
+    await bot.say("https://discordpy.readthedocs.io/en/latest/index.html")
 
 #=========================
 # non-command listeners
